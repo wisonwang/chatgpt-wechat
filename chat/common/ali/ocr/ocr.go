4007e1ae0ff570b1a7025e16e8f4c27ee4d6ff4b
Vincent <vincent202107@126.com>
2023-05-23 17:03:17 +0800

    修复ocr问题

--- chat/common/ali/ocr/ocr.go
+++ chat/common/ali/ocr/ocr.go
@@ -1,33 +1,45 @@
 package ocr
 
 import (
-	"encoding/json"
+	"net/http"
 
 	openapi "github.com/alibabacloud-go/darabonba-openapi/v2/client"
-	ocrapi20210707 "github.com/alibabacloud-go/ocr-api-20210707/client"
+
+	// 1、这里只是以ocr为例，其他能力请引入相应类目的包。包名可参考本文档上方的SDK包名称，能力名可参考对应API文档中的Action参数。例如您想使用通用分割，其文档为https://help.aliyun.com/document_detail/151960.html，可以知道该能力属于分割抠图类目，能力名称为SegmentCommonImage，那么您需要将代码中ocr20191230改为imageseg20191230，ocr-20191230改为imageseg-20191230，将RecognizeBankCard改为SegmentCommonImage。版本可以参考文档上面的SDK包信息。
+	ocr20191230 "github.com/alibabacloud-go/ocr-20191230/v3/client"
+
 	util "github.com/alibabacloud-go/tea-utils/v2/service"
 	"github.com/alibabacloud-go/tea/tea"
 )
 
-func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *ocrapi20210707.Client, _err error) {
+func CreateClient(accessKeyId *string, accessKeySecret *string) (_result *ocr20191230.Client, _err error) {
 	config := &openapi.Config{
-		// 必填，您的 AccessKey ID
-		AccessKeyId: accessKeyId,
-		// 必填，您的 AccessKey Secret
-		AccessKeySecret: accessKeySecret,
+		// 2、"YOUR_ACCESS_KEY_ID", "YOUR_ACCESS_KEY_SECRET" 的生成请参考https://help.aliyun.com/document_detail/175144.html
+		// 如果您是用的子账号AccessKey，还需要为子账号授予权限AliyunVIAPIFullAccess，请参考https://help.aliyun.com/document_detail/145025.html
+		// 您的 AccessKey ID
+		AccessKeyId: tea.String(*accessKeyId),
+		// 您的 AccessKey Secret
+		AccessKeySecret: tea.String(*accessKeySecret),
 	}
-	// 访问的域名
-	config.Endpoint = tea.String("ocr-api.cn-hangzhou.aliyuncs.com")
-	_result = &ocrapi20210707.Client{}
-	_result, _err = ocrapi20210707.NewClient(config)
-	return _result, _err
+
+	// 3、访问的域名。注意：这个地方需要求改为相应类目的域名，参考：https://help.aliyun.com/document_detail/143103.html
+	config.Endpoint = tea.String("ocr.cn-shanghai.aliyuncs.com")
+	// 4、这里只是以ocr为例，其他能力请使用相应类目的包
+	client, err := ocr20191230.NewClient(config)
+	return client, err
 }
 
-func Image2Txt(arg string, client *ocrapi20210707.Client) (res string, _err error) {
-	recognizeGeneralRequest := &ocrapi20210707.RecognizeGeneralRequest{
-		Url: tea.String(arg),
+func Image2Txt(arg string, client *ocr20191230.Client) (res string, _err error) {
+	httpClient := http.Client{}
+	file, _ := httpClient.Get(arg)
+
+	recognizeGeneralRequest := &ocr20191230.RecognizeCharacterAdvanceRequest{
+		ImageURLObject:    file.Body,
+		MinHeight:         tea.Int32(10),
+		OutputProbability: tea.Bool(false),
 	}
 	runtime := &util.RuntimeOptions{}
+
 	txt, tryErr := func() (res string, _e error) {
 		defer func() {
 			if r := tea.Recover(recover()); r != nil {
@@ -35,22 +47,16 @@ func Image2Txt(arg string, client *ocrapi20210707.Client) (res string, _err erro
 			}
 		}()
 		// 复制代码运行请自行打印 API 的返回值
-		result, _err := client.RecognizeGeneralWithOptions(recognizeGeneralRequest, runtime)
+		result, _err := client.RecognizeCharacterAdvance(recognizeGeneralRequest, runtime)
 		if _err != nil {
 			return "", _err
 		}
 
-		res = *result.Body.Data
-		type Content struct {
-			Content string `json:"content"`
-		}
-		c := &Content{}
-		err := json.Unmarshal([]byte(res), c)
-		if err != nil {
-			return "", err
+		for _, l := range result.Body.Data.Results {
+			res += *(l.Text) + "\n"
 		}
 
-		return c.Content, nil
+		return res, nil
 	}()
 
 	if tryErr != nil {
--- chat/go.mod
+++ chat/go.mod
@@ -26,10 +26,16 @@ require (
 
 require (
 	github.com/alibabacloud-go/alibabacloud-gateway-spi v0.0.4 // indirect
+	github.com/alibabacloud-go/darabonba-number v1.0.4 // indirect
 	github.com/alibabacloud-go/debug v0.0.0-20190504072949-9472017b5c68 // indirect
 	github.com/alibabacloud-go/endpoint-util v1.1.0 // indirect
+	github.com/alibabacloud-go/ocr-20191230/v3 v3.0.6 // indirect
 	github.com/alibabacloud-go/openapi-util v0.1.0 // indirect
-	github.com/alibabacloud-go/tea-utils v1.3.1 // indirect
+	github.com/alibabacloud-go/openplatform-20191219/v2 v2.0.1 // indirect
+	github.com/alibabacloud-go/tea-fileform v1.1.1 // indirect
+	github.com/alibabacloud-go/tea-oss-sdk v1.1.3 // indirect
+	github.com/alibabacloud-go/tea-oss-utils v1.1.0 // indirect
+	github.com/alibabacloud-go/tea-utils v1.3.6 // indirect
 	github.com/alibabacloud-go/tea-xml v1.1.2 // indirect
 	github.com/aliyun/credentials-go v1.1.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
@@ -85,6 +91,7 @@ require (
 	go.uber.org/automaxprocs v1.5.2 // indirect
 	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
 	google.golang.org/genproto v0.0.0-20230123190316-2c411cf9d197 // indirect
 	google.golang.org/grpc v1.54.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
